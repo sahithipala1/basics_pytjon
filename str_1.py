364f687e32abba100b92eb1a9829e2d41ddbb056
sahithipala1 <sahithipala1@gmail.com>
2022-01-10 19:52:28 -0500

    k

--- str_1.py
+++ str_1.py
@@ -1,2 +1,13 @@
-s = "Hello world"
+s = "Hello, world"
+b = s.split(",")
+print(b)
 
+a = "Hello, World!"
+b = a.split(",")
+print(len(b[1]))
+
+i = "sam wood"
+
+for x in range(len(i)):
+    if x >= 0:
+        print(x)
